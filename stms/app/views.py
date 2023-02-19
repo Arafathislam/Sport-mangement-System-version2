5830a567627b0c06eef1e7f6d32c4a61b82dc1a9
Arafathislam <islamarafath315@gmail.com>
2023-02-19 11:30:33 +0600

    working

--- stms/app/views.py
+++ stms/app/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'app/main.html')
+    return render(request,'main.html')
+
+
+def login(request):
+    return render(request,'app/login.html')
+
+
