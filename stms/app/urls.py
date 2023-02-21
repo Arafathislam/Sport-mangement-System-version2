dc86db8bd69cb0ab3c360ffa92ed820a02fbcaa7
Arafathislam <islamarafath315@gmail.com>
2023-02-21 23:37:31 +0600

    working

--- stms/app/urls.py
+++ stms/app/urls.py
@@ -4,5 +4,4 @@ from .import views
 
 urlpatterns = [
     path('',views.home,name="home"),
-     path('login/',views.login,name="home"),
 ]
--- stms/app/views.py
+++ stms/app/views.py
@@ -3,10 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'main.html')
+    return render(request,'app/index.html')
 
 
-def login(request):
-    return render(request,'app/login.html')
 
 
