e84870a23ae1544be01b126ca270b8a56742989a
Arafathislam <islamarafath315@gmail.com>
2023-02-26 23:54:23 +0600

    working

--- stms/app/views.py
+++ stms/app/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'app/index.html')
+    return render(request,'main.html')
+
+
+def allcards(request):
+    return render(request,'app/AllCards.html')
 
 
 
