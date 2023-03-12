bf4cef2ec5c97cb69bffb857d26eda983c5e212b
Arafathislam <islamarafath315@gmail.com>
2023-03-12 16:57:41 +0600

    working

--- stms/app/views.py
+++ stms/app/views.py
@@ -13,10 +13,6 @@ def home(request):
     return render(request,'app/home.html')
 
 
-def allcards(request):
-    return render(request,'app/AllCards.html')
-
-
 
 def register(request):
     # if request.method == 'POST':
@@ -70,8 +66,60 @@ def about(request):
     return render(request,'app/about.html')
 
 
+
+def allcards(request):
+    return render(request,'app/AllCards.html')
+
+
+
 def badminton_rule(request):
      return render(request,'app/badminton_rule.html')
 
 def chess_rule(request):
      return render(request,'app/chess_rule.html')
+
+
+def criket_rule(request):
+     return render(request,'app/cricket.html')
+
+def football_rule(request):
+     return render(request,'app/football.html')
+
+def highjump_rule(request):
+     return render(request,'app/highjump.html')
+
+def pc3_rule(request):
+     return render(request,'app/pc3.html')
+
+def kabaddi_rule(request):
+     return render(request,'app/kabaddi.html')
+
+def pc1_rule(request):
+     a=pc1_rule
+     context={'a':a}
+     return render(request,'app/pc1.html',context)
+
+def math_rule(request):
+     return render(request,'app/math.html')
+
+def video_rule(request):
+     return render(request,'app/video.html')
+
+def ict_rule(request):
+     return render(request,'app/ict.html')
+
+def rubiks_rule(request):
+     return render(request,'app/rubiks.html')
+
+def table_rule(request):
+     return render(request,'app/table.html')
+
+def volleyball_rule(request):
+     return render(request,'app/volleyball.html')
+
+def sprint_rule(request):
+     return render(request,'app/sprint.html')
+
+
+
+
