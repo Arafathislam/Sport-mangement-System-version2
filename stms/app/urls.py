8ac1ba1ff174c6ec35eeb9101ce9659674c9f2bc
Arafathislam <islamarafath315@gmail.com>
2023-03-12 15:31:29 +0600

    working

--- stms/app/urls.py
+++ stms/app/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('allcards/',views.allcards,name="allcards"),
     path('register/',views.register,name="register"),
     path('login/',views.loginPage,name="login"),
+    path('tournament/',views.tournament,name="tournament"),
+    path('news/',views.news,name="news"),
+    path('contact/',views.contact,name="contact"),
+    path('about/',views.about,name="about"),
+    path('badmintion_rule/',views.badminton_rule,name="badmintion_rule"),
+    path('chess_rule/',views.chess_rule,name="chess_rule"),
 ]
--- stms/app/views.py
+++ stms/app/views.py
@@ -10,7 +10,7 @@ from .models import User
 # Create your views here.
 
 def home(request):
-    return render(request,'main.html')
+    return render(request,'app/home.html')
 
 
 def allcards(request):
@@ -19,37 +19,59 @@ def allcards(request):
 
 
 def register(request):
-    if request.method == 'POST':
-        name=request.POST['name']
-        email=request.POST['email']
-        password1=request.POST['password1']
-        password2=request.POST['password2']
-        gender=request.POST['gender']
-        age=request.POST['age']
-        phone_number=request.POST['phone_number']
-        catalogue=request.POST['catalogue']
+    # if request.method == 'POST':
+    #     name=request.POST['name']
+    #     email=request.POST['email']
+    #     password1=request.POST['password1']
+    #     password2=request.POST['password2']
+    #     gender=request.POST['gender']
+    #     age=request.POST['age']
+    #     phone_number=request.POST['phone_number']
+    #     catalogue=request.POST['catalogue']
         
-        myuser=User.crea(fullname=name,email=email,password=password1,gender=gender,age=age,phone_number=phone_number,catalogue=catalogue)
-        myuser.save()
-    return render(request,'app/done.html')
+    #     myuser=User.crea(fullname=name,email=email,password=password1,gender=gender,age=age,phone_number=phone_number,catalogue=catalogue)
+    #     myuser.save()
+    return render(request,'app/register.html')
 
 
 
 def loginPage(request):
-    if request.method=='POST':
-        email = request.POST.get('email')
-        password=request.POST.get('password')
+    # if request.method=='POST':
+    #     email = request.POST.get('email')
+    #     password=request.POST.get('password')
 
-        m=User.objects.get(email=email)
+    #     m=User.objects.get(email=email)
         
-        # user=authenticate(request,email=email,password=password)
+    #     # user=authenticate(request,email=email,password=password)
 
 
-        # if user is not None:
-        #     login(request,user)
-        #     return render(request,'app/done.html')
-        # else:
-        #  return render(request,'main.html')
+    #     # if user is not None:
+    #     #     login(request,user)
+    #     #     return render(request,'app/done.html')
+    #     # else:
+    #     #  return render(request,'main.html')
 
-        if(m.email==email and m.password==password):
-            return render(request,'app/done.html')
+    #     if(m.email==email and m.password==password):
+            return render(request,'app/login.html')
+
+def tournament(request):
+    return render(request,'app/tournament.html')
+
+
+
+def news(request):
+    return render(request,'app/news.html')
+
+def contact(request):
+    return render(request,'app/contact.html')
+
+
+def about(request):
+    return render(request,'app/about.html')
+
+
+def badminton_rule(request):
+     return render(request,'app/badminton_rule.html')
+
+def chess_rule(request):
+     return render(request,'app/chess_rule.html')
